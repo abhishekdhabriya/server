2ced210e1e50ee8f81ada7b864266c260d2158ce
adhabriy <adhabriy@qualcomm.com>
2017-02-13 23:35:43 -0800

    added auth route

--- src/server/routes.js
+++ src/server/routes.js
@@ -2,4 +2,8 @@ export default function(app) {
     app.get('/', (req, res)=> {
         res.send(['hi', 'blessings']);
     });
+
+    app.get('/auth', (req, res)=> {
+        res.send('Inside auth route');
+    });
 }
